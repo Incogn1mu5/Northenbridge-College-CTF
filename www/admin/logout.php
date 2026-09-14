0cf6953ea4828cbd9b5046e2879e80063a995f35
(-_-) <112743924+Incogn1mu5@users.noreply.github.com>
2026-09-15 03:20:05 +0530

    Add files via upload

---
+++ www/admin/logout.php
@@ -0,0 +1,14 @@
+<?php
+
+session_start();
+
+unset(
+    $_SESSION['admin_logged_in'],
+    $_SESSION['admin_id'],
+    $_SESSION['admin_username']
+);
+
+session_regenerate_id(true);
+
+header('Location: index.php');
+exit;
